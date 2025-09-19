0b21432547fa349d15583a4978c27cd42ab7c78a
sujeeth2003 <sujeeth@umd.edu>
2025-09-18 22:50:33 -0400

    Create evaluate.py

---
+++ src/evaluate.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(y_true, y_pred):
+    cm = confusion_matrix(y_true, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot(cmap="Blues")
+    plt.show()
